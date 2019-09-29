66e1b40090093022d260ac68ba1d5825491f7fc1
shrenikm <shrenik95@gmail.com>
2019-09-29 16:26:00 -0700

    Build and environment updates.
    - Simplified cmake build includes. Changed to relative paths.
    - Removed macro namespaces.
    - Environment constructors.

--- src/morphac/common/include/error_macros.h
+++ src/morphac/common/include/error_macros.h
@@ -3,46 +3,41 @@
 
 #include <iostream>
 
-namespace morphac {
-namespace common {
-namespace macros {
-
 #ifndef NDEBUG
-#define MORPH_ASSERT(conditon, error_message) {\
-  if (!(condition)) {\
-    std::cerr << "------- Assertion failed -------" << std::endl;\
-    std::cerr << "Failed in file: " << __FILE__ << std::endl;\
-    std::cerr << "Failed inside function: " << __FUNCTION__ << std::endl;\
-    std::cerr << "Failed at line: " << __LINE__ << std::endl;\
-    std::cerr << "Failed with message: " << #error_message << std::endl;\
-    std::exit(-2);\
-  }\
-}
+#define MORPH_ASSERT(conditon, error_message)                               \
+  {                                                                         \
+    if (!(condition)) {                                                     \
+      std::cerr << "------- Assertion failed -------" << std::endl;         \
+      std::cerr << "Failed in file: " << __FILE__ << std::endl;             \
+      std::cerr << "Failed inside function: " << __FUNCTION__ << std::endl; \
+      std::cerr << "Failed at line: " << __LINE__ << std::endl;             \
+      std::cerr << "Failed with message: " << #error_message << std::endl;  \
+      std::exit(-2);                                                        \
+    }                                                                       \
+  }
 #endif
 
-#define MORPH_REQUIRE(condition, error_message) {\
-  if (!(condition)) {\
-    std::cerr << "------- Requirement failed -------" << std::endl;\
-    std::cerr << "Failed in file: " << __FILE__ << std::endl;\
-    std::cerr << "Failed inside function: " << __FUNCTION__ << std::endl;\
-    std::cerr << "Failed at line: " << __LINE__ << std::endl;\
-    std::cerr << "Failed with message: " << #error_message << std::endl;\
-    std::exit(-1);\
-  }\
-}
+#define MORPH_REQUIRE(condition, error_message)                             \
+  {                                                                         \
+    if (!(condition)) {                                                     \
+      std::cerr << "------- Requirement failed -------" << std::endl;       \
+      std::cerr << "Failed in file: " << __FILE__ << std::endl;             \
+      std::cerr << "Failed inside function: " << __FUNCTION__ << std::endl; \
+      std::cerr << "Failed at line: " << __LINE__ << std::endl;             \
+      std::cerr << "Failed with message: " << #error_message << std::endl;  \
+      std::exit(-1);                                                        \
+    }                                                                       \
+  }
 
-#define MORPH_WARNING(condition, warning_message) {\
-  if (condition) {\
-    std::cerr << "------- Warning -------" << std::endl;\
-    std::cerr << "Warning in file: " << __FILE__ std::endl;\
-    std::cerr << "Warning inside function: " << __FUNCTION__ << std::endl;\
-    std::cerr << "Warning at line: " << __LINE__ << std::endl;\
-    std::cerr << "Warning message: " << #warning_message << std::endl;\
-  }\
-}
-
-} // namespace macros
-} // namespace common
-} // namespace morphac
+#define MORPH_WARNING(condition, warning_message)                            \
+  {                                                                          \
+    if (condition) {                                                         \
+      std::cerr << "------- Warning -------" << std::endl;                   \
+      std::cerr << "Warning in file: " << __FILE__ std::endl;                \
+      std::cerr << "Warning inside function: " << __FUNCTION__ << std::endl; \
+      std::cerr << "Warning at line: " << __LINE__ << std::endl;             \
+      std::cerr << "Warning message: " << #warning_message << std::endl;     \
+    }                                                                        \
+  }
 
 #endif
--- src/morphac/environments/src/environment2D.cc
+++ src/morphac/environments/src/environment2D.cc
@@ -1,12 +1,35 @@
-#include "environment2D.h"
+#include "environments/include/environment2D.h"
 
 namespace morphac {
 namespace environments {
 
-Environment::Environment(int width, int height):
-  width_(width), height_(height) {}
+using Eigen::MatrixXd;
 
-} // namespace environments
-} // namespace morphac
+Environment2D::Environment2D(int width, int height, int origin_x, int origin_y)
+    : width_(width), height_(height), origin_x_(origin_x), origin_y_(origin_y) {
+  MORPH_REQUIRE(width_ > 0, "Non-positive environment width.");
+  MORPH_REQUIRE(height_ > 0, "Non-positive environment height.");
+  MORPH_REQUIRE((origin_x_ >= 0 && origin_x_ < width_),
+                "The origin's x coordinate is outside the map.")
+  MORPH_REQUIRE((origin_y_ >= 0 && origin_y_ < height_),
+                "The origin's y coordinate is outside the map.")
+  map_ = MatrixXd::Zero(height_, width_);
+}
 
+Environment2D::Environment2D(MatrixXd map, int origin_x, int origin_y)
+    : width_(map.cols()),
+      height_(map.rows()),
+      origin_x_(origin_x),
+      origin_y_(origin_y) {
+  MORPH_REQUIRE(map.cols() > 0, "Non-positive map width.");
+  MORPH_REQUIRE(map.rows() > 0, "Non-positive map height.");
+  MORPH_REQUIRE((origin_x_ >= 0 && origin_x_ < width_),
+                "The origin's x coordinate is outside the map.")
+  MORPH_REQUIRE((origin_y_ >= 0 && origin_y_ < height_),
+                "The origin's y coordinate is outside the map.")
+  map_ = map;
+}
+
+}  // namespace environments
+}  // namespace morphac
 
