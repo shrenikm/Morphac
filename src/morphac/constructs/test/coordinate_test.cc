70d8a80528b860caf9f500a04ac17dbc51c605a8
shrenikm <shrenik95@gmail.com>
2019-09-29 19:58:27 -0700

    Testing pipeline.

--- src/morphac/constructs/test/coordinate_test.cc
+++ src/morphac/constructs/test/coordinate_test.cc
@@ -1,9 +1,28 @@
 #include "gtest/gtest.h"
 
+#include "constructs/include/coordinate.h"
+
 namespace {
 
-} // namespace
+using morphac::constructs::Coordinate2D;
+
+class CoordinateTest : public ::testing::Test {
+ protected:
+  CoordinateTest() {}
+
+  void SetUp() override {}
+
+  Coordinate2D zero_coord_{Coordinate2D(0, 0)};
+};
+
+TEST_F(CoordinateTest, InitializationCheck) {
+  ASSERT_EQ(zero_coord_.get_x(), 0);
+  ASSERT_EQ(zero_coord_.get_y(), 0);
+}
+
+}  // namespace
 
 int main(int argc, char **argv) {
-  return 0;
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
