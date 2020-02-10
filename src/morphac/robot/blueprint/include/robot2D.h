77c2033cee0aec2fc8cbd6f81773cb694c2fa188
shrenikm <shrenik95@gmail.com>
2020-02-10 16:14:38 +0530

    robot params.

--- src/morphac/robot/blueprint/include/robot2D.h
+++ src/morphac/robot/blueprint/include/robot2D.h
@@ -1,6 +1,8 @@
 #ifndef ROBOT2D_H
 #define ROBOT2D_H
 
+#include <string>
+#include <unordered_map>
 #include "Eigen/Dense"
 
 #include "constructs/include/pose.h"
@@ -13,18 +15,30 @@ namespace morphac {
 namespace robot {
 namespace blueprint {
 
+class Robot2DParams {
+ public:
+  Robot2DParams(const std::string robot_name,
+                const std::unordered_map<std::string, double> robot_params);
+
+  const std::string robot_name_;
+  const std::unordered_map<std::string, double> robot_params_;
+};
+
 class Robot2D {
  public:
-  Robot2D(const morphac::mechanics::KinematicModel& kinematic_model,
+  Robot2D(const Robot2DParams& params,
+          const morphac::mechanics::KinematicModel& kinematic_model,
           const morphac::robot::blueprint::Footprint2D& footprint);
 
+  const Robot2DParams& get_params() const;
   const morphac::mechanics::KinematicModel& get_kinematic_model() const;
+  const morphac::robot::blueprint::Footprint2D get_footprint() const;
   const morphac::constructs::State& get_state() const;
   const morphac::constructs::Pose& get_pose() const;
   const morphac::constructs::Velocity& get_velocity() const;
-  const morphac::robot::blueprint::Footprint2D get_footprint() const;
 
  private:
+  const Robot2DParams& params_;
   const morphac::mechanics::KinematicModel& kinematic_model_;
   const morphac::robot::blueprint::Footprint2D footprint_;
   const std::unique_ptr<morphac::constructs::State> state_;
--- src/morphac/robot/blueprint/src/robot2D.cc
+++ src/morphac/robot/blueprint/src/robot2D.cc
@@ -5,6 +5,8 @@ namespace robot {
 namespace blueprint {
 
 using std::make_unique;
+using std::string;
+using std::unordered_map;
 
 using Eigen::MatrixXd;
 
@@ -14,24 +16,33 @@ using morphac::constructs::Velocity;
 using morphac::mechanics::KinematicModel;
 using morphac::robot::blueprint::Footprint2D;
 
-Robot2D::Robot2D(const KinematicModel& kinematic_model,
+Robot2DParams::Robot2DParams(
+    const string robot_name,
+    const unordered_map<std::string, double> robot_params)
+    : robot_name_(robot_name), robot_params_(robot_params) {}
+
+Robot2D::Robot2D(const Robot2DParams& params,
+                 const KinematicModel& kinematic_model,
                  const Footprint2D& footprint)
-    : kinematic_model_(kinematic_model),
+    : params_(params),
+      kinematic_model_(kinematic_model),
       footprint_(footprint),
       state_(make_unique<State>(kinematic_model.get_size_pose(),
                                 kinematic_model.get_size_velocity())) {}
 
+const Robot2DParams& Robot2D::get_params() const { return params_; }
+
 const KinematicModel& Robot2D::get_kinematic_model() const {
   return kinematic_model_;
 }
+
+const Footprint2D Robot2D::get_footprint() const { return footprint_; }
 
 const State& Robot2D::get_state() const { return *state_.get(); }
 
 const Pose& Robot2D::get_pose() const { return state_->get_pose(); }
 
 const Velocity& Robot2D::get_velocity() const { return state_->get_velocity(); }
-
-const Footprint2D Robot2D::get_footprint() const { return footprint_; }
 
 }  // namespace models
 }  // namespace robot
