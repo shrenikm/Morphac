000455480838f67e78174797e80d36de5a55911c
shrenikm <shrenik95@gmail.com>
2020-02-13 22:55:06 +0530

    Pose subtraction.

--- src/morphac/constructs/test/pose_test.cc
+++ src/morphac/constructs/test/pose_test.cc
@@ -77,6 +77,25 @@ TEST_F(PoseTest, Addition) {
   ASSERT_TRUE(pose3.get_pose_vector().isApprox(pose4.get_pose_vector()));
 }
 
+TEST_F(PoseTest, Subtraction) {
+  VectorXd v1(3), v2(3), q1(3), q2(3);
+  v1 << 1, 2, 3;
+  v2 << 4, 5, 6;
+  q1 << -3, -3, -3;
+  q2 << -7, -8, -9;
+
+  Pose pose1(v1);
+  Pose pose2(v2);
+  pose1 -= pose2;
+  ASSERT_TRUE(pose1.get_pose_vector().isApprox(q1));
+
+  Pose pose3 = pose1 - pose2;
+  ASSERT_TRUE(pose3.get_pose_vector().isApprox(q2));
+
+  Pose pose4 = pose2 - pose1;
+  ASSERT_TRUE(pose3.get_pose_vector().isApprox(-1 * pose4.get_pose_vector()));
+}
+
 }  // namespace
 
 int main(int argc, char **argv) {
