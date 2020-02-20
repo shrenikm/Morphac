4afce473631beaf0f982877b331fffb51a383de5
shrenikm <shrenik95@gmail.com>
2020-02-20 20:53:12 +0530

    Progress.

--- src/morphac/mechanics/models/test/tricycle_model_test.cc
+++ src/morphac/mechanics/models/test/tricycle_model_test.cc
@@ -20,11 +20,28 @@ class TricycleModelTest : public ::testing::Test {
   void SetUp() override {}
 };
 
+TEST_F(TricycleModelTest, Construction) {
+  TricycleModel tricycle_model{"tricycle_model", 1.5, 2.3};
+
+  ASSERT_EQ(tricycle_model.name, "tricycle_model");
+  ASSERT_EQ(tricycle_model.size_pose, 4);
+  ASSERT_EQ(tricycle_model.size_velocity, 0);
+  ASSERT_EQ(tricycle_model.size_input, 2);
+  ASSERT_EQ(tricycle_model.radius, 1.5);
+  ASSERT_EQ(tricycle_model.length, 2.3);
+}
+
+TEST_F(TricycleModelTest, InvalidConstruction) {
+  ASSERT_THROW(TricycleModel("invalid", -1, 1), std::invalid_argument);
+  ASSERT_THROW(TricycleModel("invalid", 0, 1), std::invalid_argument);
+  ASSERT_THROW(TricycleModel("invalid", 1, -1), std::invalid_argument);
+  ASSERT_THROW(TricycleModel("invalid", 1, -1), std::invalid_argument);
+}
+
 }  // namespace
 
 int main(int argc, char** argv) {
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
-
 
