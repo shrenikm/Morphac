8e2b4e59e6fc93071c2af43485e1e691a71845d2
shrenikm <shrenik95@gmail.com>
2020-03-18 01:48:36 -0700

    Starting pose binding

--- src/morphac/constructs/binding/binding_pose.cc
+++ src/morphac/constructs/binding/binding_pose.cc
@@ -1,1 +1,25 @@
 #include "pybind11/pybind11.h"
+#include "pybind11/eigen.h"
+
+#include "constructs/include/pose.h"
+
+namespace morphac {
+namespace constructs {
+namespace bindings {
+
+namespace py = pybind11;
+
+using Eigen::VectorXd;
+using morphac::constructs::Pose;
+
+PYBIND11_MODULE(binding_pose, m) {
+  py::class_<Pose>(m, "Pose")
+    .def(py::init<const int>())
+    .def(py::init<const VectorXd&>())
+    .def("get_size", &Pose::get_size);
+}
+
+
+}  // namespace bindings
+}  // namespace constructs
+}  // namespace morphac
