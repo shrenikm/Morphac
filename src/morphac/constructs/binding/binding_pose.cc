2d2aed846e0be3f74da10a896f69e9182425d5ef
shrenikm <shrenik95@gmail.com>
2020-03-19 01:13:37 -0700

    Overloading getitem. Still some issues with reference not being returned.

--- src/morphac/constructs/binding/binding_pose.cc
+++ src/morphac/constructs/binding/binding_pose.cc
@@ -1,6 +1,6 @@
-#include "pybind11/pybind11.h"
 #include "pybind11/eigen.h"
 #include "pybind11/operators.h"
+#include "pybind11/pybind11.h"
 
 #include "constructs/include/pose.h"
 
@@ -15,22 +15,26 @@ using morphac::constructs::Pose;
 
 PYBIND11_MODULE(binding_pose, m) {
   py::class_<Pose>(m, "Pose")
-    .def(py::init<const int>())
-    .def(py::init<const VectorXd&>())
-    .def(py::self += py::self)
-    .def(py::self + py::self)
-    .def(py::self -= py::self)
-    .def(py::self - py::self)
-    .def(py::self *= double())
-    .def(py::self * double())
-    .def(double() * py::self)
-    .def("get_size", &Pose::get_size)
-    .def("get_pose_vector", &Pose::get_pose_vector)
-    .def("set_pose_vector", &Pose::set_pose_vector)
-    .def("is_empty", &Pose::IsEmpty)
-    .def("create_like", &Pose::CreateLike);
+      .def(py::init<const int>())
+      .def(py::init<const VectorXd&>())
+      .def(py::self += py::self)
+      .def(py::self + py::self)
+      .def(py::self -= py::self)
+      .def(py::self - py::self)
+      .def(py::self *= double())
+      .def(py::self * double())
+      .def(double() * py::self)
+      .def("__getitem__",
+           [](const Pose& pose, const int index) {
+             return pose.get_pose_vector()(index);
+           },
+           py::is_operator(), py::return_value_policy::reference)
+      .def("get_size", &Pose::get_size)
+      .def("get_pose_vector", &Pose::get_pose_vector)
+      .def("set_pose_vector", &Pose::set_pose_vector)
+      .def("is_empty", &Pose::IsEmpty)
+      .def("create_like", &Pose::CreateLike);
 }
-
 
 }  // namespace bindings
 }  // namespace constructs
