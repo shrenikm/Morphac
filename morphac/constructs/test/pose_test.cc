f875a59eb2a0277975753f05ac89a75e29ef6c92
shrenikm <shrenik95@gmail.com>
2020-04-19 18:51:53 -0700

    Updated pose test

--- morphac/constructs/test/pose_test.cc
+++ morphac/constructs/test/pose_test.cc
@@ -59,6 +59,19 @@ TEST_F(PoseTest, ConstPose) {
   }
 }
 
+TEST_F(PoseTest, EmptyConstruction) {
+  Pose pose;
+
+  // Assert emptiness.
+  ASSERT_TRUE(pose.IsEmpty());
+  ASSERT_TRUE(Pose{}.IsEmpty());
+
+  // Accessors are invalid for empty Pose
+  ASSERT_THROW(pose.get_pose_vector(), std::logic_error);
+  ASSERT_THROW(pose(0), std::logic_error);
+  ASSERT_THROW(pose.set_pose_vector(VectorXd::Random(0)), std::logic_error);
+}
+
 TEST_F(PoseTest, Sizes) {
   ASSERT_EQ(pose1_->get_size(), 3);
   ASSERT_EQ(pose2_->get_size(), 4);
@@ -146,131 +159,130 @@ TEST_F(PoseTest, SetPoseAt) {
   ASSERT_THROW((*pose1_)(3) = 1, std::out_of_range);
 }
 
-//
-// TEST_F(PoseTest, Addition) {
-//  VectorXd p1(3), p2(3), d1(3), d2(3);
-//  p1 << 1, 2, 3;
-//  p2 << 4, 5, 6;
-//  d1 << 5, 7, 9;
-//  d2 << 9, 12, 15;
-//
-//  Pose pose1(p1);
-//  Pose pose2(p2);
-//
-//  // Trivial addition.
-//  pose1 += Pose::CreateLike(pose1);
-//  ASSERT_TRUE(pose1.get_pose_vector().isApprox(p1));
-//
-//  pose1 = pose1 + Pose::CreateLike(pose1);
-//  ASSERT_TRUE(pose1.get_pose_vector().isApprox(p1));
-//
-//  pose1 += pose2;
-//  ASSERT_TRUE(pose1.get_pose_vector().isApprox(d1));
-//
-//  Pose pose3 = pose1 + pose2;
-//  ASSERT_TRUE(pose3.get_pose_vector().isApprox(d2));
-//
-//  Pose pose4 = pose2 + pose1;
-//  ASSERT_TRUE(pose3.get_pose_vector().isApprox(pose4.get_pose_vector()));
-//}
-//
-// TEST_F(PoseTest, Subtraction) {
-//  VectorXd p1(3), p2(3), d1(3), d2(3);
-//  p1 << 1, 2, 3;
-//  p2 << 4, 5, 6;
-//  d1 << -3, -3, -3;
-//  d2 << -7, -8, -9;
-//
-//  Pose pose1(p1);
-//  Pose pose2(p2);
-//
-//  // Trivial subtraction.
-//  pose1 -= Pose::CreateLike(pose1);
-//  ASSERT_TRUE(pose1.get_pose_vector().isApprox(p1));
-//
-//  pose1 = pose1 - Pose::CreateLike(pose1);
-//  ASSERT_TRUE(pose1.get_pose_vector().isApprox(p1));
-//
-//  pose1 -= pose2;
-//  ASSERT_TRUE(pose1.get_pose_vector().isApprox(d1));
-//
-//  Pose pose3 = pose1 - pose2;
-//  ASSERT_TRUE(pose3.get_pose_vector().isApprox(d2));
-//
-//  Pose pose4 = pose2 - pose1;
-//  ASSERT_TRUE(pose3.get_pose_vector().isApprox(-1 * pose4.get_pose_vector()));
-//}
-//
-// TEST_F(PoseTest, Multiplication) {
-//  VectorXd p1(3), p2(3), d1(3), d2(3);
-//  p1 << 1, 2, 3;
-//  p2 << 4, 5, 6;
-//  d1 << 2, 4, 6;
-//  d2 << -4, -5, -6;
-//
-//  Pose pose1(p1);
-//  Pose pose2(p2);
-//
-//  // Trivial multiplication.
-//  pose1 = pose1 * 1.0;
-//  ASSERT_TRUE(pose1.get_pose_vector().isApprox(p1));
-//  ASSERT_TRUE((0.0 * pose1).get_pose_vector().isApprox(VectorXd::Zero(3)));
-//
-//  pose1 *= 2.0;
-//  ASSERT_TRUE(pose1.get_pose_vector().isApprox(d1));
-//
-//  Pose pose3 = pose2 * -1.0;
-//  Pose pose4 = -1 * pose2;
-//  ASSERT_TRUE(pose3.get_pose_vector().isApprox(d2));
-//  ASSERT_TRUE(pose4.get_pose_vector().isApprox(d2));
-//}
-//
-// TEST_F(PoseTest, StringRepresentation) {
-//  // Testing that the << operator is overloaded properly.
-//  // We don't test the actual string representation.
-//  ostringstream os;
-//  os << pose1_;
-//
-//  // Multiple pose object representations in the stream.
-//  os << " " << pose3_ << std::endl;
-//}
-//
-//// TEST_F(PoseTest, EmptyConstruction) {
-//  Pose pose;
-//
-//  // Assert emptiness.
-//  ASSERT_TRUE(pose.IsEmpty());
-//
-//  // Accessors are invalid for empty Pose
-//  ASSERT_THROW(pose.get_pose_vector(), std::logic_error);
-//  ASSERT_THROW(pose(0), std::logic_error);
-//  ASSERT_THROW(pose.set_pose_vector(VectorXd::Random(0)), std::logic_error);
-//}
-//
-// TEST_F(PoseTest, EmptyPoseOperations) {
-//  // Basic operations on empty velocities must result in empty velocities.
-//  Pose pose1, pose2;
-//
-//  Pose pose_add = pose1 + pose2;
-//  ASSERT_TRUE(pose_add.IsEmpty());
-//
-//  Pose pose_sub = pose1 - pose2;
-//  ASSERT_TRUE(pose_sub.IsEmpty());
-//
-//  Pose pose_mult = pose1 * 7.0;
-//  ASSERT_TRUE(pose_mult.IsEmpty());
-//}
-//
-// TEST_F(PoseTest, CreateLike) {
-//  Pose pose1 = Pose::CreateLike(pose1_);
-//  Pose pose2 = Pose::CreateLike(pose3_);
-//
-//  ASSERT_EQ(pose1.get_size(), 3);
-//  ASSERT_TRUE(pose1.get_pose_vector().isApprox(VectorXd::Zero(3)));
-//
-//  ASSERT_EQ(pose2.get_size(), 6);
-//  ASSERT_TRUE(pose2.get_pose_vector().isApprox(VectorXd::Zero(6)));
-//}
+TEST_F(PoseTest, Addition) {
+  VectorXd p1(3), p2(3), d1(3), d2(3);
+  p1 << 1, 2, 3;
+  p2 << 4, -5, 6;
+  d1 << 5, -3, 9;
+  d2 << 9, -8, 15;
+
+  Pose pose1{1, 2, 3};
+  Pose pose2(p2);
+
+  // Trivial addition.
+  pose1 += Pose::CreateLike(pose1);
+  ASSERT_TRUE(pose1.get_pose_vector().isApprox(p1));
+
+  pose1 = pose1 + Pose::CreateLike(pose1);
+  ASSERT_TRUE(pose1.get_pose_vector().isApprox(p1));
+
+  pose1 += pose2;
+  ASSERT_TRUE(pose1.get_pose_vector().isApprox(d1));
+
+  // Commutativity.
+  Pose pose3 = pose1 + pose2;
+  ASSERT_TRUE(pose3.get_pose_vector().isApprox(d2));
+
+  Pose pose4 = pose2 + pose1;
+  ASSERT_TRUE(pose3.get_pose_vector().isApprox(pose4.get_pose_vector()));
+
+  // Empty addition.
+  Pose empty_pose1, empty_pose2{};
+
+  Pose empty_pose_add = empty_pose1 + empty_pose2;
+  ASSERT_TRUE(empty_pose_add.IsEmpty());
+}
+
+TEST_F(PoseTest, Subtraction) {
+  VectorXd p1(3), p2(3), d1(3), d2(3);
+  p1 << 1, 2, 3;
+  p2 << 4, -5, 6;
+  d1 << -3, 7, -3;
+  d2 << -7, 12, -9;
+
+  Pose pose1({1, 2, 3});
+  Pose pose2(p2);
+
+  // Trivial subtraction.
+  pose1 -= Pose::CreateLike(pose1);
+  ASSERT_TRUE(pose1.get_pose_vector().isApprox(p1));
+
+  pose1 = pose1 - Pose::CreateLike(pose1);
+  ASSERT_TRUE(pose1.get_pose_vector().isApprox(p1));
+
+  pose1 -= pose2;
+  ASSERT_TRUE(pose1.get_pose_vector().isApprox(d1));
+
+  Pose pose3 = pose1 - pose2;
+  ASSERT_TRUE(pose3.get_pose_vector().isApprox(d2));
+
+  Pose pose4 = pose2 - pose1;
+  ASSERT_TRUE(pose3.get_pose_vector().isApprox(-1 * pose4.get_pose_vector()));
+
+  // Empty subtraction.
+  Pose empty_pose1, empty_pose2{};
+
+  Pose empty_pose_sub = empty_pose1 - empty_pose2;
+  ASSERT_TRUE(empty_pose_sub.IsEmpty());
+}
+
+TEST_F(PoseTest, Multiplication) {
+  VectorXd p1(3), p2(3), d1(3), d2(3);
+  p1 << 1, 2, 3;
+  p2 << 4, -5, 6;
+  d1 << 2, 4, 6;
+  d2 << -6, 7.5, -9;
+
+  Pose pose1({1, 2, 3});
+  Pose pose2(p2);
+
+  // Trivial multiplication.
+  pose1 = pose1 * 1.0;
+  ASSERT_TRUE(pose1.get_pose_vector().isApprox(p1));
+  ASSERT_TRUE((0.0 * pose1).get_pose_vector().isApprox(VectorXd::Zero(3)));
+
+  pose1 *= 2.0;
+  ASSERT_TRUE(pose1.get_pose_vector().isApprox(d1));
+
+  // Commutativity.
+  Pose pose3 = pose2 * -1.5;
+  Pose pose4 = -1.5 * pose2;
+  ASSERT_TRUE(pose3.get_pose_vector().isApprox(d2));
+  ASSERT_TRUE(pose4.get_pose_vector().isApprox(d2));
+
+  // Empty multiplication.
+  Pose empty_pose1, empty_pose2{};
+
+  Pose empty_pose_mult1 = empty_pose1 * 7.0;
+  Pose empty_pose_mult2 = empty_pose2 * 7.0;
+  ASSERT_TRUE(empty_pose_mult1.IsEmpty());
+  ASSERT_TRUE(empty_pose_mult2.IsEmpty());
+}
+
+TEST_F(PoseTest, StringRepresentation) {
+  // Testing that the << operator is overloaded properly.
+  // We don't test the actual string representation.
+  ostringstream os;
+  os << (*pose1_);
+
+  // Multiple pose object representations in the stream.
+  os << " " << (*pose2_) << std::endl;
+}
+
+TEST_F(PoseTest, CreateLike) {
+  Pose pose1 = Pose::CreateLike(*pose1_);
+  Pose pose2 = Pose::CreateLike(*pose2_);
+  Pose pose3 = Pose::CreateLike(*pose3_);
+
+  ASSERT_EQ(pose1.get_size(), 3);
+  ASSERT_TRUE(pose1.get_pose_vector().isApprox(VectorXd::Zero(3)));
+
+  ASSERT_EQ(pose2.get_size(), 4);
+  ASSERT_TRUE(pose2.get_pose_vector().isApprox(VectorXd::Zero(4)));
+
+  ASSERT_EQ(pose3.get_size(), 5);
+  ASSERT_TRUE(pose3.get_pose_vector().isApprox(VectorXd::Zero(5)));
+}
 
 }  // namespace
 
