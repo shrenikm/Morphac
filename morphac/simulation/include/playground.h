5ae53a36e39f81b9b1d211103a527213c71b2649
shrenikm <shrenik95@gmail.com>
2020-06-10 02:02:47 -0700

    playground state init. Need to remove 2D

--- morphac/simulation/include/playground.h
+++ morphac/simulation/include/playground.h
@@ -14,6 +14,25 @@ struct PlaygroundSpec {
   int gui_height;
 };
 
+// class PlaygroundState {
+// public:
+//  PlaygroundState(const morphac::environments::Environment2D& environment2D);
+//
+//  // Delete copy constructor. We don't want to copy the state to avoid
+//  incurring
+//  // the cost of copying the environment object.
+//  PlaygroundState(const PlaygroundState& playground_state) = delete;
+//
+//  // Also deleting copy assignment.
+//  PlaygroundState& operator=(const PlaygroundState& playground_state) =
+//  delete;
+//
+// private:
+//  morphac::environments::Environment2D environment2D_;
+//  std::unordered_map<string, morphac::robot::blueprint::Robot2D>
+//  robot_oracle_;
+//};
+
 class Playground {
  public:
   Playground(const PlaygroundSpec& spec,
