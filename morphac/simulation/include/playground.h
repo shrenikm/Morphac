9dec84777d66fc9073362094e6d0fa4e8ccdef1f
shrenikm <shrenik95@gmail.com>
2020-06-11 23:39:33 -0700

    playground now uses playground state to maintain the state

--- morphac/simulation/include/playground.h
+++ morphac/simulation/include/playground.h
@@ -1,7 +1,10 @@
 #ifndef PLAYGROUND_H
 #define PLAYGROUND_H
 
+#include <unordered_map>
+
 #include "environments/include/environment.h"
+#include "robot/blueprint/include/robot.h"
 
 namespace morphac {
 namespace simulation {
@@ -14,24 +17,28 @@ struct PlaygroundSpec {
   int gui_height;
 };
 
-// class PlaygroundState {
-// public:
-//  PlaygroundState(const morphac::environments::Environment& environment);
-//
-//  // Delete copy constructor. We don't want to copy the state to avoid
-//  incurring
-//  // the cost of copying the environment object.
-//  PlaygroundState(const PlaygroundState& playground_state) = delete;
-//
-//  // Also deleting copy assignment.
-//  PlaygroundState& operator=(const PlaygroundState& playground_state) =
-//  delete;
-//
-// private:
-//  morphac::environments::Environment environment_;
-//  std::unordered_map<string, morphac::robot::blueprint::Robot>
-//  robot_oracle_;
-//};
+class PlaygroundState {
+ public:
+  PlaygroundState(const morphac::environments::Environment& environment);
+
+  // Delete copy constructor. We don't want to copy the state to avoid
+  // incurring the cost of copying the environment object.
+  PlaygroundState(const PlaygroundState& playground_state) = delete;
+
+  // Also deleting copy assignment.
+  PlaygroundState& operator=(const PlaygroundState& playground_state) = delete;
+
+  const morphac::environments::Environment& get_environment() const;
+  const std::unordered_map<int, morphac::robot::blueprint::Robot>&
+  get_robot_oracle() const;
+
+  void set_environment(const morphac::environments::Environment& environment);
+  void AddRobot(int id, const morphac::robot::blueprint::Robot& robot);
+
+ private:
+  morphac::environments::Environment environment_;
+  std::unordered_map<int, morphac::robot::blueprint::Robot> robot_oracle_;
+};
 
 class Playground {
  public:
@@ -44,9 +51,11 @@ class Playground {
   // Delete copy assignment.
   Playground& operator=(const Playground& playground) = delete;
 
+  const PlaygroundState& get_state() const;
+
  private:
   const PlaygroundSpec spec_;
-  morphac::environments::Environment environment_;
+  PlaygroundState playground_state_;
 };
 
 }  // namespace simulation
--- morphac/simulation/src/playground.cc
+++ morphac/simulation/src/playground.cc
@@ -3,11 +3,37 @@
 namespace morphac {
 namespace simulation {
 
+using std::unordered_map;
+
 using morphac::environments::Environment;
+using morphac::robot::blueprint::Robot;
+
+PlaygroundState::PlaygroundState(const Environment& environment)
+    : environment_(environment) {}
+
+const Environment& PlaygroundState::get_environment() const {
+    return environment_;
+}
+
+const unordered_map<int, Robot>& PlaygroundState::get_robot_oracle() const {
+    return robot_oracle_;
+}
+
+void PlaygroundState::set_environment(const Environment& environment) {
+    environment_ = environment;
+}
+
+void PlaygroundState::AddRobot(int id, const Robot& robot) {
+    robot_oracle_.insert({id, robot});
+}
 
 Playground::Playground(const PlaygroundSpec& spec,
                        const Environment& environment)
-    : spec_(spec), environment_(environment) {}
+    : spec_(spec), playground_state_(environment) {}
+
+const PlaygroundState& Playground::get_state() const {
+    return playground_state_;
+}
 
 }  // namespace simulation
 }  // namespace morphac
