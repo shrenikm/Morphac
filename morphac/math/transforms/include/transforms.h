0776bccfad3bc08d4ffd093a58985c2b7c88cde8
shrenikm <shrenik95@gmail.com>
2020-08-10 22:34:54 -0700

    canvas <-> world coord functions init

--- morphac/math/transforms/include/transforms.h
+++ morphac/math/transforms/include/transforms.h
@@ -17,6 +17,20 @@ const Eigen::MatrixXd TransformationMatrix(const double angle,
 const Eigen::MatrixXd TransformationMatrix(
     const double angle, const morphac::constructs::Coordinate& translation);
 
+const Eigen::Vector2d CanvasToWorld(const Eigen::Vector2d& coord,
+                                    const double resolution);
+
+const morphac::constructs::Coordinate CanvasToWorld(
+    const morphac::constructs::Coordinate& coord, const double resolution);
+
+const Eigen::Vector2d WorldToCanvas(
+    const Eigen::Vector2d& coord, const double resolution,
+    const Eigen::Vector2d& canvas_size = Eigen::Vector2d::Zero(0));
+
+const morphac::constructs::Coordinate WorldToCanvas(
+    const morphac::constructs::Coordinate& coord, const double resolution,
+    const Eigen::Vector2d& canvas_size = Eigen::Vector2d::Zero(0));
+
 }  // namespace transforms
 }  // namespace math
 }  // namespace morphac
