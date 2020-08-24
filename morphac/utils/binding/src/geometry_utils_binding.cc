bcf3d45aaeb0986a33836ddb614b2186125de13c
shrenikm <shrenik95@gmail.com>
2020-08-23 19:40:39 -0700

    bindings

--- morphac/utils/binding/src/geometry_utils_binding.cc
+++ morphac/utils/binding/src/geometry_utils_binding.cc
@@ -15,9 +15,17 @@ using morphac::utils::CreateRoundedRectangularPolygon;
 
 void define_geometry_utils_binding(py::module& m) {
   m.def("create_arc", &CreateArc, py::arg("start_angle"), py::arg("end_angle"),
-        py::arg("radius"), py::arg("angular_resolution"), py::arg("center"));
+        py::arg("radius"), py::arg("angular_resolution"),
+        py::arg("center") = Vector2d::Zero());
   m.def("create_circular_polygon", &CreateCircularPolygon, py::arg("radius"),
-        py::arg("angular_resolution"), py::arg("center"));
+        py::arg("angular_resolution"), py::arg("center") = Vector2d::Zero());
+  m.def("create_rectangular_polygon", &CreateRectangularPolygon,
+        py::arg("size_x"), py::arg("size_y"), py::arg("angle"),
+        py::arg("center") = Vector2d::Zero());
+  m.def("create_rounded_rectangular_polygon", &CreateRoundedRectangularPolygon,
+        py::arg("size_x"), py::arg("size_y"), py::arg("angle"),
+        py::arg("radius"), py::arg("angular_resolution"),
+        py::arg("center") = Vector2d::Zero());
 }
 
 }  // namespace binding
