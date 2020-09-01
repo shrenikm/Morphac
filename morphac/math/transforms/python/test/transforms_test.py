b3b1683736fc40ff4b959ca9fc49ae49a483e0ef
shrenikm <shrenik95@gmail.com>
2020-08-31 23:41:54 -0700

    Update pytests

--- morphac/math/transforms/python/test/transforms_test.py
+++ morphac/math/transforms/python/test/transforms_test.py
@@ -108,19 +108,22 @@ def test_invalid_transform_points(generate_points_list):
 
 def test_canvas_to_world():
 
-    world_coord = canvas_to_world(canvas_coord=[100, 200], resolution=0.03)
+    world_coord = canvas_to_world(
+        canvas_coord=[100, 200], resolution=0.03, canvas_size=(200, 200))
 
     assert world_coord.size == 2
     assert world_coord.ndim == 1
     assert world_coord.dtype == np.float64
-    assert np.allclose(world_coord, [3., 6.])
 
 
 def test_canvas_to_world_multiple_points():
 
     set_standard_testing_random_seed()
     world_coords = canvas_to_world(
-        canvas_coords=np.random.randint(200, size=(10, 2)), resolution=0.1)
+        canvas_coords=np.random.randint(200, size=(10, 2)),
+        resolution=0.1,
+        canvas_size=(200, 200)
+    )
 
     assert world_coords.shape == (10, 2)
     assert world_coords.dtype == np.float64
@@ -128,38 +131,19 @@ def test_canvas_to_world_multiple_points():
 
 def test_world_to_canvas():
 
-    canvas_coord = world_to_canvas(world_coord=[-2., 17.], resolution=0.01)
-
-    assert canvas_coord.size == 2
-    assert canvas_coord.ndim == 1
-    assert canvas_coord.dtype == np.int32
-
-    # Make sure that the coordinate is valid.
-    assert np.allclose(canvas_coord, [-200, 1700])
-
-    # Provide a canvas size and make sure that invalid coordinates are returned
-    # as [-1, -1]
-
     canvas_coord = world_to_canvas(
-        world_coord=[-2., 17.], resolution=0.01, canvas_size=(500, 500))
+        world_coord=[2., 17.], resolution=0.01, canvas_size=(200, 200))
 
     assert canvas_coord.size == 2
     assert canvas_coord.ndim == 1
     assert canvas_coord.dtype == np.int32
-    assert np.allclose(canvas_coord, [-1, -1])
 
 
 def test_world_to_canvas():
     set_standard_testing_random_seed()
     world_coords = np.random.randn(10, 2)
-    canvas_coords = world_to_canvas(world_coords=world_coords, resolution=0.1)
-
-    assert canvas_coords.shape == (10, 2)
-    assert canvas_coords.dtype == np.int32
-
-    # Make sure that the interface with the canvas size works.
     canvas_coords = world_to_canvas(
-        world_coords=world_coords, resolution=0.1, canvas_size=(500, 500))
+        world_coords=world_coords, resolution=0.1, canvas_size=(200, 200))
 
     assert canvas_coords.shape == (10, 2)
     assert canvas_coords.dtype == np.int32
