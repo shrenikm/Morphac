643a864841e8be3aa521645d50fbe6c48b79dc5b
shrenikm <shrenik95@gmail.com>
2020-09-19 21:58:33 -0700

    Done

--- build.sh
+++ build.sh
@@ -3,11 +3,14 @@
 usage="$(basename "$0") [-h] [command]
 
 options:
-    -h      Help text
+    -h            Help text
     clean         Cleans the project build directory
     build         Builds the project using CMake
     install       Builds and installs the project using CMake
-    cleanbuild    Cleans the project and builds it again"
+    cleanbuild    Cleans the project and builds it again
+    test          Run cpp tests"
+
+num_cores=`getconf _NPROCESSORS_ONLN`
 
 while getopts 'h' option; do
     case "$option" in
@@ -33,23 +36,27 @@ then
     cd build
     # TODO: Add Cmake -D options here.
     cmake ..
+    make -j$num_cores
 elif [[ $1 = "install" ]]
 then
-    echo "Building the project ..."
+    echo "Building and installing the project ..."
     cd build
     cmake ..
-    echo "Installing the project ..."
-    make install
+    make install -j$num_cores
 elif [[ $1 = "cleanbuild" ]]
 then
     echo "Cleaning the build directory ..."
     rm -rf build/*
-    echo "Building the project ..."
+    echo "Building and installing project ..."
     cd build
     cmake ..
     echo "Installing the project ..."
-    make install
-
+    make install -j$num_cores
+elif [[ $1 = "test" ]]
+then
+    echo "Running cpp tests ..."
+    cd build
+    make test -j$num_cores
 else
     echo "Invalid option. See the usage with -h"
     exit 1
