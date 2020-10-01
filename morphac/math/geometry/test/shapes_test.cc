f5571fbc90385808e5bc84a6f4ad61b099e9ffbf
shrenikm <shrenik95@gmail.com>
2020-09-30 21:23:44 -0700

    ok

--- morphac/math/geometry/test/shapes_test.cc
+++ morphac/math/geometry/test/shapes_test.cc
@@ -24,12 +24,12 @@ class ShapesTest : public ::testing::Test {
   void SetUp() override {}
 
   // Standard shapes
-  ArcShape arc_shape_{Point(-1., 2.), M_PI / 4, M_PI, 2.};
-  CircleShape circle_shape_{Point(-1., 2.), 2.};
-  RectangleShape rectangle_shape_{Point(-1., 2.), 3., 1.5, M_PI / 4};
-  RoundedRectangleShape rounded_rectangle_shape_{Point(-1., 2.), 3., 1.5,
-                                                 M_PI / 4, 0.5};
-  TriangleShape triangle_shape_{Point(-1., 2.), 3., 1.5, M_PI / 4.};
+  ArcShape arc_shape_{M_PI / 4, M_PI, 2., Point(-1., 2.)};
+  CircleShape circle_shape_{2., Point(-1., 2.)};
+  RectangleShape rectangle_shape_{3., 1.5, M_PI / 4, Point(-1., 2.)};
+  RoundedRectangleShape rounded_rectangle_shape_{3., 1.5, M_PI / 4, 0.5,
+                                                 Point(-1., 2.)};
+  TriangleShape triangle_shape_{3., 1.5, M_PI / 4., Point(-1., 2.)};
 };
 
 TEST_F(ShapesTest, ArcShape) {
@@ -39,14 +39,16 @@ TEST_F(ShapesTest, ArcShape) {
   ASSERT_TRUE(IsEqual(arc_shape_.radius, 2.));
 }
 
+TEST_F(ShapesTest, DefaultCenterArcShape) {
+  ASSERT_TRUE(ArcShape(0., M_PI / 2, 2.).center.isApprox(Vector2d::Zero()));
+}
+
 TEST_F(ShapesTest, InvalidArcShape) {
   // Invalid radius.
-  ASSERT_THROW(ArcShape(Point::Zero(), 0., M_PI / 2, -0.1),
-               std::invalid_argument);
+  ASSERT_THROW(ArcShape(0., M_PI / 2, -0.1), std::invalid_argument);
 
   // Invalid start and end angles (equal).
-  ASSERT_THROW(ArcShape(Point::Zero(), M_PI / 2., M_PI / 2, 1.),
-               std::invalid_argument);
+  ASSERT_THROW(ArcShape(M_PI / 2., M_PI / 2, 1.), std::invalid_argument);
 }
 
 TEST_F(ShapesTest, CircleShape) {
@@ -54,9 +56,13 @@ TEST_F(ShapesTest, CircleShape) {
   ASSERT_TRUE(IsEqual(circle_shape_.radius, 2.));
 }
 
+TEST_F(ShapesTest, DefaultCenterCircleShape) {
+  ASSERT_TRUE(CircleShape(2.).center.isApprox(Vector2d::Zero()));
+}
+
 TEST_F(ShapesTest, InvalidCircleShape) {
   // Invalid radius.
-  ASSERT_THROW(CircleShape(Point::Zero(), -0.1), std::invalid_argument);
+  ASSERT_THROW(CircleShape(-0.1), std::invalid_argument);
 }
 
 TEST_F(ShapesTest, RectangleShape) {
@@ -66,13 +72,15 @@ TEST_F(ShapesTest, RectangleShape) {
   ASSERT_TRUE(IsEqual(rectangle_shape_.angle, M_PI / 4));
 }
 
+TEST_F(ShapesTest, DefaultCenterRectangleShape) {
+  ASSERT_TRUE(RectangleShape(1., 1., 0.).center.isApprox(Vector2d::Zero()));
+}
+
 TEST_F(ShapesTest, InvalidRectangleShape) {
   // Invalid size_x.
-  ASSERT_THROW(RectangleShape(Point::Zero(), -0.1, 1., 0.),
-               std::invalid_argument);
+  ASSERT_THROW(RectangleShape(-0.1, 1., 0.), std::invalid_argument);
   // Invalid size_y.
-  ASSERT_THROW(RectangleShape(Point::Zero(), 1., -0.1, 0.),
-               std::invalid_argument);
+  ASSERT_THROW(RectangleShape(1., -0.1, 0.), std::invalid_argument);
 }
 
 TEST_F(ShapesTest, RoundedRectangleShape) {
@@ -83,21 +91,21 @@ TEST_F(ShapesTest, RoundedRectangleShape) {
   ASSERT_TRUE(IsEqual(rounded_rectangle_shape_.radius, 0.5));
 }
 
+TEST_F(ShapesTest, DefaultCenterRoundedRectangleShape) {
+  ASSERT_TRUE(
+      RoundedRectangleShape(1., 1., 0., 0.1).center.isApprox(Vector2d::Zero()));
+}
+
 TEST_F(ShapesTest, InvalidRoundedRectangleShape) {
   // Invalid size_x.
-  ASSERT_THROW(RoundedRectangleShape(Point::Zero(), -0.1, 1., 0., 0.1),
-               std::invalid_argument);
+  ASSERT_THROW(RoundedRectangleShape(-0.1, 1., 0., 0.1), std::invalid_argument);
   // Invalid size_y.
-  ASSERT_THROW(RoundedRectangleShape(Point::Zero(), 1., -0.1, 0., 0.1),
-               std::invalid_argument);
+  ASSERT_THROW(RoundedRectangleShape(1., -0.1, 0., 0.1), std::invalid_argument);
   // Invalid radius.
-  ASSERT_THROW(RoundedRectangleShape(Point::Zero(), 1., 1., 0., -0.1),
-               std::invalid_argument);
+  ASSERT_THROW(RoundedRectangleShape(1., 1., 0., -0.1), std::invalid_argument);
   // The radius must also be smaller than half of the sizes.
-  ASSERT_THROW(RoundedRectangleShape(Point::Zero(), 3., 2., 0., 1.1),
-               std::invalid_argument);
-  ASSERT_THROW(RoundedRectangleShape(Point::Zero(), 2., 3., 0., 1.1),
-               std::invalid_argument);
+  ASSERT_THROW(RoundedRectangleShape(3., 2., 0., 1.1), std::invalid_argument);
+  ASSERT_THROW(RoundedRectangleShape(2., 3., 0., 1.1), std::invalid_argument);
 }
 
 TEST_F(ShapesTest, TriangleShape) {
@@ -107,13 +115,15 @@ TEST_F(ShapesTest, TriangleShape) {
   ASSERT_TRUE(IsEqual(triangle_shape_.angle, M_PI / 4));
 }
 
+TEST_F(ShapesTest, DefaultCenterTriangleShape) {
+  ASSERT_TRUE(TriangleShape(1., 1., 0.).center.isApprox(Vector2d::Zero()));
+}
+
 TEST_F(ShapesTest, InvalidTriangleShape) {
   // Invalid base.
-  ASSERT_THROW(TriangleShape(Point::Zero(), -0.1, 1., 0.),
-               std::invalid_argument);
+  ASSERT_THROW(TriangleShape(-0.1, 1., 0.), std::invalid_argument);
   // Invalid height.
-  ASSERT_THROW(TriangleShape(Point::Zero(), 1., -0.1, 0.),
-               std::invalid_argument);
+  ASSERT_THROW(TriangleShape(1., -0.1, 0.), std::invalid_argument);
 }
 
 }  // namespace
