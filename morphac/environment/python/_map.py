ccbbdecfb78338cc7825bbfdef394281d56069fa
shrenikm <shrenik95@gmail.com>
2020-10-03 02:32:50 -0700

    map obstacles init

--- morphac/environment/python/_map.py
+++ morphac/environment/python/_map.py
@@ -8,16 +8,21 @@ from morphac.constants.colors import MapColors
 from morphac.environment import Map
 from morphac.math.geometry import CircleShape, RectangleShape
 from morphac.math.transforms import world_to_canvas
+from morphac.utils.cv2_utils import canvas_to_cv2
 
 
 # Note that the obstacle adding functions do not mutate the input map.
 # Rather it creates a new map with the added obstacle.
 # Hence the name 'evolve'
-def evolve_map_with_circular_obstacle(map, circle_shape):
-    map_data = np.copy(map.data)
-    center = world_to_canvas(circle_shape.center, map.resolution, map.data.shape)
-    radius = world_to_canvas(circle_shape.radius, map.resolution)
-    cv2.circle(map_data, center, radius, MapConstants.OBSTACLE, thickness=-1)
+def evolve_map_with_circular_obstacle(env_map, circle_shape):
+    env_map_data = np.copy(env_map.data)
+    center = canvas_to_cv2(
+        world_to_canvas(circle_shape.center, env_map.resolution, env_map.data.shape)
+    )
+    radius = world_to_canvas(circle_shape.radius, env_map.resolution)
+    cv2.circle(env_map, center, radius, MapConstants.OBSTACLE, thickness=-1)
+
+    return env_map.evolve(env_map_data)
 
 
 def evolve_map_with_rectangular_obstacle(map, rectangle_shape):
--- morphac/utils/python/cv2_utils.py
+++ morphac/utils/python/cv2_utils.py
@@ -6,10 +6,14 @@ def canvas_to_cv2(canvas_coords):
     # Opencv coordinates are canvas coordinates with interchanged x and y components.
     # This is because points requires the x and y axes to be the regular axes,
     # with the origin at the top left.
-    canvas_coords = np.array(canvas_coords, dtype=np.int)
+    canvas_coords = np.array(canvas_coords, dtype=np.int32)
     if canvas_coords.ndim == 1:
+        if len(canvas_coords) != 2:
+            raise MorphacLogicError("Invalid coordinate size.")
         return canvas_coords[::-1]
     elif canvas_coords.ndim == 2:
+        if canvas_coords.shape[1] != 2:
+            raise MorphacLogicError("Invalid coordinate size.")
         return canvas_coords[:, ::-1]
     else:
         raise MorphacLogicError("Invalid coordinate dimensions.")
