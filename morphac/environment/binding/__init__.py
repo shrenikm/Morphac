c3ba4a3308096becbe33cdefaf9f3bd24ff1adff
shrenikm <shrenik95@gmail.com>
2020-10-04 18:29:31 -0700

    oops

--- morphac/environment/binding/__init__.py
+++ morphac/environment/binding/__init__.py
@@ -2,6 +2,5 @@ from ._binding_environment_python import Map
 
 from morphac.environment._map import (
     evolve_map_with_circular_obstacle,
-    evolve_map_with_rectangular_obstacle,
     evolve_map_with_polygonal_obstacle,
 )
