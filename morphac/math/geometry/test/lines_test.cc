7687778b0fb4571c6e54d7ec257c41c630a023f8
shrenikm <shrenik95@gmail.com>
2020-10-20 23:43:36 -0700

    projection test1

--- morphac/math/geometry/test/lines_test.cc
+++ morphac/math/geometry/test/lines_test.cc
@@ -8,6 +8,7 @@ using std::atan2;
 using std::isinf;
 using std::numeric_limits;
 using std::ostringstream;
+using std::sqrt;
 using std::tan;
 
 using morphac::common::aliases::Epsilon;
@@ -16,6 +17,7 @@ using morphac::common::aliases::Point;
 using morphac::math::geometry::AreLinesParallel;
 using morphac::math::geometry::AreLinesPerpendicular;
 using morphac::math::geometry::ComputeLineSpec;
+using morphac::math::geometry::ComputePointProjection;
 using morphac::math::geometry::LineSpec;
 using morphac::math::geometry::PointProjection;
 
@@ -171,6 +173,69 @@ TEST_F(LinesTest, AreLinesPerpendicular) {
                                     Point(-10., 15.), Point(2., 3.)));
   ASSERT_TRUE(AreLinesPerpendicular(Point(0., 0.), Point(-1., 1.),
                                     Point(-23., -20.), Point(-1., 2.)));
+}
+
+TEST_F(LinesTest, ComputePointProjectionSegment) {
+  // Test project computation to a line segment.
+  // Defining three segments for testing.
+  Point start_point1(0., 0.), end_point1(1., 0.);
+  Point start_point2(0., 3.), end_point2(0., -2.);
+  Point start_point3(-1., -1.), end_point3(4., 4.);
+
+  // Testing different projections for these three segments.
+  PointProjection point_projection1 =
+      ComputePointProjection(Point(-1., 0.), start_point1, end_point1);
+  ASSERT_EQ(point_projection1.alpha, -1.);
+  ASSERT_EQ(point_projection1.distance, 0.);
+  ASSERT_TRUE(point_projection1.projection.isApprox(Point(-1., 0.)));
+
+  PointProjection point_projection2 =
+      ComputePointProjection(Point(5., 5.), start_point1, end_point1);
+  ASSERT_EQ(point_projection2.alpha, 5.);
+  ASSERT_EQ(point_projection2.distance, 5.);
+  ASSERT_TRUE(point_projection2.projection.isApprox(Point(5., 0.)));
+
+  PointProjection point_projection3 =
+      ComputePointProjection(Point(0.1, 0.), start_point1, end_point1);
+  ASSERT_EQ(point_projection3.alpha, 0.1);
+  ASSERT_EQ(point_projection3.distance, 0.);
+  ASSERT_TRUE(point_projection3.projection.isApprox(Point(0.1, 0.)));
+
+  PointProjection point_projection4 =
+      ComputePointProjection(Point(0, 4.), start_point2, end_point2);
+  ASSERT_EQ(point_projection4.alpha, -0.2);
+  ASSERT_EQ(point_projection4.distance, 0.);
+  ASSERT_TRUE(point_projection4.projection.isApprox(Point(0., 4.)));
+
+  PointProjection point_projection5 =
+      ComputePointProjection(Point(-2, -7.), start_point2, end_point2);
+  ASSERT_EQ(point_projection5.alpha, 2.);
+  ASSERT_EQ(point_projection5.distance, 2.);
+  ASSERT_TRUE(point_projection5.projection.isApprox(Point(0., -7.)));
+
+  PointProjection point_projection6 =
+      ComputePointProjection(Point(0., 0.), start_point2, end_point2);
+  ASSERT_EQ(point_projection6.alpha, 0.6);
+  ASSERT_EQ(point_projection6.distance, 0.);
+  ASSERT_TRUE(point_projection6.projection.isApprox(Point(0., 0.)));
+
+  PointProjection point_projection7 =
+      ComputePointProjection(Point(-6., -6.), start_point3, end_point3);
+  ASSERT_EQ(point_projection7.alpha, -1.);
+  ASSERT_EQ(point_projection7.distance, 0.);
+  ASSERT_TRUE(point_projection7.projection.isApprox(Point(-6., -6.)));
+
+  PointProjection point_projection8 =
+      ComputePointProjection(Point(0., 10.), start_point3, end_point3);
+  ASSERT_EQ(point_projection8.alpha, 1.2);
+  ASSERT_EQ(point_projection8.distance, sqrt(50.));
+  ASSERT_TRUE(point_projection8.projection.isApprox(Point(5., 5.)));
+
+  PointProjection point_projection9 =
+      ComputePointProjection(Point(0., 0.), start_point3, end_point3);
+  ASSERT_EQ(point_projection9.alpha, 0.2);
+  ASSERT_EQ(point_projection9.distance, 0.);
+  ASSERT_TRUE(point_projection9.projection.isApprox(Point(0., 0.)));
 }
 
 }  // namespace
